5bdb27c546f83bc3fdf34c8150074750577424f5
Gleb-is-404 <89805208+Gleb-is-404@users.noreply.github.com>
2021-09-13 15:31:48 +0300

    Update PROJ.py

--- PROJ.py
+++ PROJ.py
@@ -1,1 +1,15 @@
-print('rlk hj')
+from random import randint
+y = 1
+v = 0
+g = 10
+while y:
+	print('очки',v)
+	print('макс квадрат',g)
+	x = randint(g-9, g)
+	s = int(input(x**2))
+	if s == x:
+		v += 1
+	if v == g:
+		g += 10
+	else:
+		y = 0
